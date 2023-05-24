c3fa24c0e0d808bc9b3d8adcfea42163a21aa202
Jason Yuan <jasonyuan0130@gmail.com>
2023-05-24 11:54:34 +0930

    feat: dashboard with create new and revoke api key

---
+++ src/helpers/revoke-api-key.ts
@@ -0,0 +1,13 @@
+export const revokeApiKey = async () => {
+  const res = await fetch('/api/api-key/revoke', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  const data = (await res.json()) as { error?: string }
+
+  if (data.error) {
+    throw new Error(data.error)
+  }
+}
